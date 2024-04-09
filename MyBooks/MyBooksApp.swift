7bc8b6e6a30dfe5721eba41d8418d4a461e62d4d
Justin Grimes <grimesajustin@gmail.com>
2024-04-09 10:23:25 -0400

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Justin Grimes on 4/9/24.
+//  Copyright © 2024 Justin Grimes. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
